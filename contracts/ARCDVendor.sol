9405572956470bd11b7195d0f1efca2b9af58447
Christopher David <chris@arcade.city>
2018-03-20 15:23:38 -0500

    Change buy function to use external token; tests passing

--- contracts/ARCDVendor.sol
+++ contracts/ARCDVendor.sol
@@ -7,19 +7,27 @@ import "./zeppelin/math/SafeMath.sol";
 contract ARCDVendor is Ownable {
 
   uint256 public buyPrice;
+  StandardToken public myToken;
 
-  address public constant ARCD_TOKEN_ADDRESS = 0xb581E3a7dB80fBAA821AB39342E9Cbfd2ce33c23;
+  /* address public constant ARCD_TOKEN_ADDRESS = 0xb581E3a7dB80fBAA821AB39342E9Cbfd2ce33c23; */
+
+  function ARCDCrowdsale () public {
+    myToken = new StandardToken();
+  }
 
   function setPrices(uint256 newBuyPrice) onlyOwner {
     buyPrice = newBuyPrice;
   }
 
+  function setToken(address tokenAddress) onlyOwner {
+    myToken = StandardToken(tokenAddress);
+  }
+
   function buy() payable returns (uint amount){
-    amount = msg.value / buyPrice;                    // calculates the amount
-    require(balanceOf[this] >= amount);               // checks if it has enough to sell
-    balanceOf[msg.sender] += amount;                  // adds the amount to buyer's balance
-    balanceOf[this] -= amount;                        // subtracts amount from seller's balance
-    Transfer(this, msg.sender, amount);               // execute an event reflecting the change
+    amount = msg.value / buyPrice;                    // Calculates the amount of tokens attempting to be purchased
+    require(myToken.balanceOf(this) >= amount);       // Checks if this contract has enough token to sell
+    myToken.transfer(msg.sender, amount);             // Sends the amount of tokens to the buyer
+    /* Transfer(this, msg.sender, amount);               // execute an event reflecting the change */
     return amount;                                    // ends function and returns
   }
 

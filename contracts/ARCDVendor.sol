e6db6de4bc0e04a6bc81d46a552aca39b0ea1284
Christopher David <chris@arcade.city>
2018-03-21 16:21:09 -0500

    Now works great on Ropsten - added event, fixed decimals

--- contracts/ARCDVendor.sol
+++ contracts/ARCDVendor.sol
@@ -6,6 +6,24 @@ import "./zeppelin/math/SafeMath.sol";
 
 contract ARCDVendor is Ownable {
 
+  using SafeMath for uint256;
+
+  // metadata
+  uint256 public constant decimals = 18;
+
+  event BuyAttempt(
+    address indexed _from,
+    uint256 indexed _buyprice,
+    uint256 indexed _msgvalue,
+    uint256 _amounttoreturn,
+    uint256 balance
+  );
+
+  event TestEvent(
+    address indexed _from
+  );
+
+  uint256 public amount;
   uint256 public buyPrice;
   StandardToken public myToken;
 
@@ -13,18 +31,18 @@ contract ARCDVendor is Ownable {
 
   function ARCDVendor () public {
     myToken = StandardToken(ARCD_TOKEN_ADDRESS);
-    buyPrice = 12500000000000;  // 0.0000125 per 1 ETH?
+    buyPrice = 12500000000000;  // 0.0000125 per 1 ARCD
   }
 
   function setPrices(uint256 newBuyPrice) public onlyOwner {
     buyPrice = newBuyPrice;
   }
 
-  function buy() public payable returns (uint amount){
-    amount = msg.value / buyPrice;                    // Calculates the amount of tokens attempting to be purchased
-    require(myToken.balanceOf(this) >= amount);       // Checks if this contract has enough token to sell
-    myToken.transfer(msg.sender, amount);             // Sends the amount of tokens to the buyer
-    return amount;                                    // ends function and returns
+  function () public payable {
+    amount = msg.value / buyPrice;                            // Calculates the amount of tokens attempting to be purchased
+    require(myToken.balanceOf(this) >= amount);               // Checks if this contract has enough token to sell
+    BuyAttempt(msg.sender, buyPrice, msg.value, amount * 10**decimals, myToken.balanceOf(this));  // Fire an event
+    myToken.transfer(msg.sender, amount * 10**decimals);      // Sends the amount of tokens to the buyer
   }
 
 }

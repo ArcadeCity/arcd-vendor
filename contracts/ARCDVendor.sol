f89704406ce44a17659b520e7939d977389773a8
Christopher David <chris@arcade.city>
2018-03-21 13:34:19 -0500

    Prepare for Ropsten test

--- contracts/ARCDVendor.sol
+++ contracts/ARCDVendor.sol
@@ -9,25 +9,21 @@ contract ARCDVendor is Ownable {
   uint256 public buyPrice;
   StandardToken public myToken;
 
-  /* address public constant ARCD_TOKEN_ADDRESS = 0xb581E3a7dB80fBAA821AB39342E9Cbfd2ce33c23; */
+  address public constant ARCD_TOKEN_ADDRESS = 0x7Ba509375e2Fae3a0860a2A0b82bD975CB30E6b0; // Ropsten
 
-  function ARCDCrowdsale () public {
-    myToken = new StandardToken();
+  function ARCDVendor () public {
+    myToken = StandardToken(ARCD_TOKEN_ADDRESS);
+    buyPrice = 12500000000000;  // 0.0000125 per 1 ETH?
   }
 
   function setPrices(uint256 newBuyPrice) public onlyOwner {
     buyPrice = newBuyPrice;
   }
 
-  function setToken(address tokenAddress) public onlyOwner {
-    myToken = StandardToken(tokenAddress);
-  }
-
   function buy() public payable returns (uint amount){
     amount = msg.value / buyPrice;                    // Calculates the amount of tokens attempting to be purchased
     require(myToken.balanceOf(this) >= amount);       // Checks if this contract has enough token to sell
     myToken.transfer(msg.sender, amount);             // Sends the amount of tokens to the buyer
-    /* Transfer(this, msg.sender, amount);               // execute an event reflecting the change */
     return amount;                                    // ends function and returns
   }
 
--- truffle.js
+++ truffle.js
@@ -1,6 +1,5 @@
 module.exports = {
-  // See <http://truffleframework.com/docs/advanced/configuration>
-  // for more about customizing your Truffle configuration!
+  // http://truffleframework.com/docs/advanced/configuration
   networks: {
     development: {
       host: "127.0.0.1",

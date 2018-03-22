1fb17250476ff3985550bc8bdf94fa17aa06e2e3
Christopher David <chris@arcade.city>
2018-03-22 12:45:54 -0500

    Adds mainnet addresses

--- contracts/ARCDVendor.sol
+++ contracts/ARCDVendor.sol
@@ -15,6 +15,9 @@ contract ARCDVendor is Ownable {
   address public constant ARCD_TOKEN_ADDRESS = 0x7Ba509375e2Fae3a0860a2A0b82bD975CB30E6b0; // Ropsten
   address public constant ETH_DEPOSIT_ADDRESS = 0xfB5234e724b2d44Ab118C3d3d9c000fD4E475509; // Ropsten
 
+  // address public constant ARCD_TOKEN_ADDRESS = 0xb581E3a7dB80fBAA821AB39342E9Cbfd2ce33c23; // Mainnet
+  // address public constant ETH_DEPOSIT_ADDRESS = 0x3b2470E99b402A333a82eE17C3244Ff04C79Ec6F; // Mainnet
+
   StandardToken public myToken;
 
   function ARCDVendor () public {
@@ -33,11 +36,11 @@ contract ARCDVendor is Ownable {
 
   function () public payable {
     require (msg.value != 0);
-    uint256 tokensTryingToBuy = msg.value.mul(tokenExchangeRate);
-    require (tokensTryingToBuy >= minBuyTokens);                          // return ETH if tokens is less than the min amount
-    require (myToken.balanceOf(this) >= tokensTryingToBuy);
+    uint256 tokensTryingToBuy = msg.value.mul(tokenExchangeRate);   // How much you looking for?
+    require (tokensTryingToBuy >= minBuyTokens);              // Return ETH if tokens is less than the min amount
+    require (myToken.balanceOf(this) >= tokensTryingToBuy);   // Make sure this contract has enough ARCD
     myToken.transfer(msg.sender, tokensTryingToBuy);          // Sends the amount of tokens to the buyer
-    ETH_DEPOSIT_ADDRESS.transfer(msg.value);
+    ETH_DEPOSIT_ADDRESS.transfer(msg.value);                  // Forward ETH to the final deposit address
   }
 
 }
